7ec41b665cfe8fa3e5e1e8f1c56087d86f68e710
khanmn <khanmohdfaisal75@gmail.com>
2021-10-05 22:56:30 +0530

    prepared for heroku deployment

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const userRouter = require('./routes/userRoutes');
 const noteRouter = require('./routes/noteRoute');
@@ -8,14 +9,28 @@ const noteRouter = require('./routes/noteRoute');
 const app = express();
 
 app.use(express.json());
+app.enable('trust-proxy');
+
+app.use(cookieParser());
+
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
 
 app.use(morgan('dev'));
+
+app.use((req, res, next) => {
+  console.log(req.cookie);
+  next();
+});
 
 app.use('/api/users', userRouter);
 app.use('/api/notes', noteRouter);
 
 app.use('*', (req, res, next) => {
-  console.log(req);
   res.status(401).json({ status: 'failed', error: 'Invalid request' });
 });
 
